a2a21b554db6ca7183f24ea8d9755e90a97073b5
deepaksharma302 <deepak.sharma302@gmail.com>
2015-08-26 17:28:57 +0530

    Merge branch 'master1' of https://github.com/deepaksharma302/RestaurantDemo
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/mentobile/restaurantdemo/AddressFragment.java
@@ -0,0 +1,36 @@
+package mentobile.restaurantdemo;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class AddressFragment extends Fragment {
+
+    private RadioGroup rgAddress;
+    private RadioButton rbSelectAdrress;
+
+    public AddressFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_address, container, false);
+        rgAddress = (RadioGroup) view.findViewById(R.id.address_rg_address);
+
+        return view;
+    }
+}
